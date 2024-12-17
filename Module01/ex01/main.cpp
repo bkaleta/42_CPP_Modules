3ad404629e39e8c61e7ee2aa3ae9684b3e04d06c
Kaletas <bkaleta64@gmail.com>
2024-12-17 23:51:14 +0100

    update

--- Module01/ex01/main.cpp
+++ Module01/ex01/main.cpp
@@ -2,5 +2,18 @@
 
 int	main()
 {
-	
+	Zombie *horde;
+	int		N = 10, i = 0;
+
+	horde = zombieHorde(N, "Legion");
+	if (horde)
+	{
+		while (i < N)
+		{
+			horde[i].announce();
+			i++;
+		}
+		delete [] horde;
+	}
+	return (0);
 }
