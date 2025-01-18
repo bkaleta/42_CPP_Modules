63d0a81fbbdd9346d1f430cf534eb8ae94d1be9e
Kaletas <bkaleta64@gmail.com>
2025-01-18 21:19:43 +0100

    update

---
+++ Module02/ex02/Fixed.hpp
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 01:33:36 by bkaleta           #+#    #+#             */
+/*   Updated: 2025/01/18 21:19:17 by bkaleta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+class Fixed
+{
+private:
+	int							 fix_nbr;
+	static const int 			bits = 8;
+public:
+	Fixed(); // default constructor
+	Fixed(const Fixed &other); // copy constructor
+	Fixed(const int to_fix);
+	Fixed(const float to_fix);
+	Fixed &operator = (const Fixed &other); // copy operator
+	~Fixed(); // desctructor 
+
+	bool	operator > (const Fixed &other) const;
+	bool	operator < (const Fixed &other) const;
+	bool	operator >= (const Fixed &other) const;
+	bool	operator <= (const Fixed &other) const;
+	bool	operator == (const Fixed &other) const;
+	bool	operator != (const Fixed &other) const;
+
+	Fixed	operator + (const Fixed &other) const;
+	Fixed	operator - (const Fixed &other) const;
+	Fixed	operator * (const Fixed &other) const;
+	Fixed	operator / (const Fixed &other) const;
+
+	Fixed	operator ++ (const Fixed &other) const;
+	Fixed	operator -- (const Fixed &other) const;
+	Fixed	&operator ++ (const Fixed &other) const;
+	Fixed	&operator -- (const Fixed &other) const;
+
+	static Fixed &min(Fixed &a, Fixed &b);
+	static const Fixed &min(Fixed const &a, Fixed const &b);
+	static Fixed &max(Fixed &a, Fixed &b);
+	static const Fixed &max(Fixed const &a, Fixed const &b);
+	
+	int	getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
+};
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
+
+#endif
---
+++ Module02/ex02/main.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 01:33:15 by bkaleta           #+#    #+#             */
+/*   Updated: 2025/01/18 21:02:43 by bkaleta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) 
+{
+	Fixed a;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+	
+	std::cout << b << std::endl;
+	
+	std::cout << Fixed::max( a, b ) << std::endl;
+return 0;
+}
