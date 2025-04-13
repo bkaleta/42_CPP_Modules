7dce12f8623b86c879aed2b4868937eabed9a76d
Kaletas <bkaleta64@gmail.com>
2025-04-13 22:31:57 +0200

    update

--- Module04/ex02/main.cpp
+++ Module04/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 13:24:47 by bkaleta           #+#    #+#             */
-/*   Updated: 2025/03/28 14:59:47 by bkaleta          ###   ########.fr       */
+/*   Updated: 2025/04/13 22:30:17 by bkaleta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,33 @@
 
 int	main()
 {
-	Animal	*array[100];
-	int		i = 0;
+	Animal *a;
+	Dog	dog;
+	Cat	cat;
 
-	while (i < 50)
-		array[i++] = new Dog();
-	while (i < 100)
-		array[i++] = new Cat();
-	std::cout << "\n\n\n Array created\n";
-	array[42]->makeSound();
-	array[96]->makeSound();
+	a = &dog;
+	a->makeSound();
+	a = &cat;
+	a->makeSound();
 
-	std::cout << "\n\n\n Sounds done";
-	i = 0;
-	while (i < 100)
+	Animal *pets[2];
+	pets[0] = new Dog();
+	pets[1] = new Cat();
+	
+	int i = 0;
+	while (i < 2)
 	{
-        delete array[i];
-		i++;		
+		pets[i]->makeSound();
+		i++;
 	}
+	i = 0;
+	while (i < 2)
+	{
+		delete pets[i];
+		i++;
+	}
+	// Animal nochance;
+	// nochance.makeSound();
 	return (0);
 }
 
