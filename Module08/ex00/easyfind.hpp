6efc59ff559c63337818d3fd8c32be688334587f
Kaletas <bkaleta64@gmail.com>
2025-08-28 20:42:40 +0200

    update

--- Module08/ex00/easyfind.hpp
+++ Module08/ex00/easyfind.hpp
@@ -3,10 +3,15 @@
 
 # include <algorithm>
 # include <exception>
+# include <stdexcept>
 
-
-
-
+template <typename T>
+typename T::const_iterator easyfind(const T &container, int value) {
+	typename T::const_iterator it = find(container.begin(), container.end(), value);
+	if (it == container.end())
+		throw std::out_of_range("Out of range!");
+	return (it);
+}
 
 
 #endif
---
+++ Module08/ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkaleta <bkaleta@student.42warsaw.pl>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/28 20:30:54 by bkaleta           #+#    #+#             */
+/*   Updated: 2025/08/28 20:42:01 by bkaleta          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "easyfind.hpp"
+#include <list>
+#include <iostream>
+
+int main() {
+	std::list<int> values;
+	
+	values.push_back(1);
+	values.push_back(2);
+	values.push_back(7);
+	values.push_back(4);
+	
+	try {
+		std::list<int>::const_iterator it = easyfind(values, 3);
+		std::cout << *it << std::endl;
+	} catch (std::out_of_range &warrning) {
+		std::cerr << warrning.what() << std::endl;
+	}
+}
