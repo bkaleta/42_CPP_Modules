83d83a9895172599c88dd1da7945e733279f52ba
Kaletas <bkaleta64@gmail.com>
2025-09-07 15:28:16 +0200

    update

---
+++ Module09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,18 @@
+#ifndef BITCOINEXCHANGE_HPP
+# define BITCOINEXCHANGE_HPP
+
+# include <iostream>
+# include <fstream>
+# include <string>
+
+class BitcoinExchange
+{
+private:
+	
+public:
+};
+
+
+
+
+#endif
